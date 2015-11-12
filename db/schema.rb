c128aff0bece501ed67f115833b004838a8f667f
njyothiguduru <nagajyothig@quinnox.com>
2015-11-12 02:05:22 -0800

    Added Code for Products with CSS and Updated the Home Screen

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027065649) do
+ActiveRecord::Schema.define(version: 20151029102041) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151027065649) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
